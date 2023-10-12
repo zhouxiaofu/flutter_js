99a26eac8a3e135e35f1905b85e76023d5a20665
xiaofu <417035570@qq.com>
2023-10-12 21:11:19 +0800

    升级httpClient22

--- lib/extensions/xhr.dart
+++ lib/extensions/xhr.dart
@@ -321,6 +321,7 @@ extension JavascriptRuntimeXhrExtension on JavascriptRuntime {
         }
         http.Response response;
         debugPrint("发送请求++------------${pendingCall.url}");
+        debugPrint("测试000--");
         debugPrint(
             "测试-----------${await (await (await HttpClient().getUrl(Uri.parse("https://www.lewen0001.com/search?searchkey=%E6%98%9F%E9%97%A8"))).close()).transform(utf8.decoder).join()}");
         try {
